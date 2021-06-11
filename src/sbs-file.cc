d2ba7998956a0b2d75600bbe3cf180f2f9661746
eduardodoria <doriaeduardo@gmail.com>
2021-06-10 22:31:13 -0300

    Fixed buffer overflow error

--- src/sbs-file.cc
+++ src/sbs-file.cc
@@ -220,7 +220,7 @@ bool supershader::generate_sbs(const std::vector<spirvcross_t>& spirvcrossvec, c
         ofs.write((char *) &refl_size, sizeof(uint32_t));
 
         sbs_chunk_refl refl;
-        strcpy(refl.name, args.output_basename.c_str());
+        strcpy(refl.name, args.output_basename.substr(0, 32).c_str());
         refl.num_inputs = num_inputs;
         refl.num_uniform_blocks = num_ubs;
         refl.num_textures = num_textures;
@@ -229,8 +229,8 @@ bool supershader::generate_sbs(const std::vector<spirvcross_t>& spirvcrossvec, c
 
         for (int a = 0; a < num_inputs; a++){
             sbs_refl_input refl_input;
-            strcpy(refl_input.name, spirvcrossvec[i].inputs[a].name.c_str());
-            strcpy(refl_input.semantic_name, spirvcrossvec[i].inputs[a].semantic_name.c_str());
+            strcpy(refl_input.name, spirvcrossvec[i].inputs[a].name.substr(0, 32).c_str());
+            strcpy(refl_input.semantic_name, spirvcrossvec[i].inputs[a].semantic_name.substr(0, 32).c_str());
             refl_input.location = spirvcrossvec[i].inputs[a].location;
             refl_input.semantic_index = spirvcrossvec[i].inputs[a].semantic_index;
             refl_input.type = get_vertex_type(spirvcrossvec[i].inputs[a].type);
@@ -240,7 +240,7 @@ bool supershader::generate_sbs(const std::vector<spirvcross_t>& spirvcrossvec, c
 
         for (int a = 0; a < num_ubs; a++){
             sbs_refl_uniformblock refl_uniformbuffer;
-            strcpy(refl_uniformbuffer.name, spirvcrossvec[i].uniform_blocks[a].name.c_str());
+            strcpy(refl_uniformbuffer.name, spirvcrossvec[i].uniform_blocks[a].name.substr(0, 32).c_str());
             refl_uniformbuffer.set = spirvcrossvec[i].uniform_blocks[a].set;
             refl_uniformbuffer.binding = spirvcrossvec[i].uniform_blocks[a].binding;
             refl_uniformbuffer.size_bytes = spirvcrossvec[i].uniform_blocks[a].size_bytes;
@@ -250,7 +250,7 @@ bool supershader::generate_sbs(const std::vector<spirvcross_t>& spirvcrossvec, c
 
         for (int a = 0; a < num_textures; a++){
             sbs_refl_texture refl_texture;
-            strcpy(refl_texture.name, spirvcrossvec[i].textures[a].name.c_str());
+            strcpy(refl_texture.name, spirvcrossvec[i].textures[a].name.substr(0, 32).c_str());
             refl_texture.set = spirvcrossvec[i].textures[a].set;
             refl_texture.binding = spirvcrossvec[i].textures[a].binding;
             refl_texture.type = get_texture_format(spirvcrossvec[i].textures[a].type);
